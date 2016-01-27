209787683ff8371abbb22c5f52701e254adc7e33
Jeff Corpac <jcorpac@gmail.com>
2016-01-27 13:04:07 -0600

    End of Chapter 9

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,47 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Jeff on 1/27/2016.
+ */
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+
+        // Create Temp objects, delete this code later
+        for(int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime # " + i);
+            crime.setIsSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public static CrimeLab get(Context context) {
+        if(sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for(Crime crime:mCrimes) {
+            if (crime.getId() == id) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,12 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
-
+/**
+ * Created by Jeff on 1/27/2016.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
