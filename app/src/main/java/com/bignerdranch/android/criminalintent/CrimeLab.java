e6662f23637064da8c979a30ab0ffe080fd3f7ed
Jeff Corpac <jcorpac@gmail.com>
2016-02-11 14:02:40 -0600

    Chapter 13 Complete Added Menu item for Adding new crime Added menu item for showing/hiding subtitle

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -15,14 +15,6 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-
-        // Create Temp objects, delete this code later
-        for(int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime # " + i);
-            crime.setIsSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
     }
 
     public static CrimeLab get(Context context) {
@@ -35,6 +27,8 @@ public class CrimeLab {
     public List<Crime> getCrimes() {
         return mCrimes;
     }
+
+    public void addCrime(Crime c) { mCrimes.add(c); }
 
     public Crime getCrime(UUID id) {
         for(Crime crime:mCrimes) {
